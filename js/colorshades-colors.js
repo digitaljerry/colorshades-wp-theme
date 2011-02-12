2998748322b56b5b6db0d3f1707e9b6d88923388
Jernej Zorec <jernejz@jabook.interkontrakt.com>
2011-02-12 17:31:03 +0100

    Added js folder. Remove some more translation files.

---
+++ js/colorshades-colors.js
@@ -0,0 +1,15 @@
+var themes = {"default":      ["#468966","#FFF0A5","#FFB03B","#B64926","#8E2800"],
+            "dolores":      ["E8E595","D0A825","40627C","26393D","FFFAE4"],
+            "grannysmith":  ["85DB18","CDE855","F5F6D4","A7C520","493F0B"],
+            "indian":       ["C98B2F","803C27","C56520","E1B41B","807916"],
+            "jambajuice":   ["ca3995","f58220","ffdf05","bed73d","61bc46"],
+            "sandstone":    ["E6E2AF","A7A37E","EFECCA","046380","002F2F"],
+            "olive":        ["B4AF91","787746","40411E","32331D"],
+            "coolrunnings": ["FFF8E3","CCCC9F","33332D","9FB4CC","DB4105"],
+            "bw":           ["FFF8E3","33332D"],
+            "neutralblue":  ["FCFFF5","D1DBBD","91AA9D","3E606F","193441"],
+            "bananaflambe": ["F0C755","E2AD3B","BF5C00","901811","5C110F"],
+            "denim":        ["4F5D73","65758D","313E57"],
+            "winters-end":  ["E6D1AA","587265","3D3A24","342F29","A14727"],
+            "sumatra":      ["1A1D20","667371","2E3538","57495A","3A2C3A"]
+};
